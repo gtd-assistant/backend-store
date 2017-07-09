a320e601da5fbbcc079347eea7a259ea91cfc547
Alvaro Garcia <derecerca@gmail.com>
2017-07-09 21:21:28 +0200

    Add pom with dependencies. Starter kit with: data-rest, hateoas, jdbc, security, lombok, boot; h2

---
+++ src/test/java/io/alvarogarcia7/petprojects/gtdassistant/backend/DataSourceConfig.java
@@ -0,0 +1,25 @@
+package io.alvarogarcia7.petprojects.gtdassistant.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
+import javax.sql.DataSource;
+
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(H2)
+                .setScriptEncoding("UTF-8")
+                .ignoreFailedDrops(true)
+                .addScript("schema.sql")
+                .addScripts("user_data.sql", "country_data.sql")
+                .build();
+    }
+}
