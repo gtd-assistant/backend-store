528dedc9123a5a2fb7adb3989d6e79b98414225f
Alvaro Garcia <derecerca@gmail.com>
2017-07-17 01:01:35 +0200

    Refactor: prefer a more functional style

--- src/main/java/io/alvarogarcia7/petprojects/gtdassistant/backend/EventBus.java
+++ src/main/java/io/alvarogarcia7/petprojects/gtdassistant/backend/EventBus.java
@@ -6,7 +6,6 @@ import org.springframework.util.MultiValueMap;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.function.Consumer;
 
 
@@ -14,10 +13,9 @@ public class EventBus {
     private final MultiValueMap<Class, Consumer> subscribers = new LinkedMultiValueMap<>();
 
     public void publish(Event event) {
-        List<Consumer> s = subscribers.getOrDefault(event.getClass(), Collections.emptyList());
-        for (Consumer consumer : s) {
-            consumer.accept(event);
-        }
+        subscribers
+                .getOrDefault(event.getClass(), Collections.emptyList())
+                .forEach(callback -> callback.accept(event));
     }
 
     public <T> void subscribe(Class<T> event, Consumer<T> callback) {
