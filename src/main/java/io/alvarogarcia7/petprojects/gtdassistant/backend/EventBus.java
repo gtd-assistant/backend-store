07ae4f9cdae9c7233560aa313b7d05bbc7aaa416
Alvaro Garcia <derecerca@gmail.com>
2017-07-17 01:01:55 +0200

    Refactor: reorder methods

--- src/main/java/io/alvarogarcia7/petprojects/gtdassistant/backend/EventBus.java
+++ src/main/java/io/alvarogarcia7/petprojects/gtdassistant/backend/EventBus.java
@@ -12,15 +12,15 @@ import java.util.function.Consumer;
 public class EventBus {
     private final MultiValueMap<Class, Consumer> subscribers = new LinkedMultiValueMap<>();
 
-    public void publish(Event event) {
-        subscribers
-                .getOrDefault(event.getClass(), Collections.emptyList())
-                .forEach(callback -> callback.accept(event));
-    }
-
     public <T> void subscribe(Class<T> event, Consumer<T> callback) {
         this.subscribers
                 .computeIfAbsent(event, (x) -> new ArrayList<>())
                 .add(callback);
+    }
+
+    public void publish(Event event) {
+        subscribers
+                .getOrDefault(event.getClass(), Collections.emptyList())
+                .forEach(callback -> callback.accept(event));
     }
 }
