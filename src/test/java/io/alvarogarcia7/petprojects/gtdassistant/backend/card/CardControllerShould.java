6582ee4f8746d392d80736d8c343213191c8c90e
Alvaro Garcia <derecerca@gmail.com>
2017-07-17 00:24:44 +0200

    Refactor: rename class

--- src/test/java/io/alvarogarcia7/petprojects/gtdassistant/backend/card/CardControllerShould.java
+++ src/test/java/io/alvarogarcia7/petprojects/gtdassistant/backend/card/CardControllerShould.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
-public class CardsControllerShould {
+public class CardControllerShould {
 
     @Mock
     EventBus eventBus;
